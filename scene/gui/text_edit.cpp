5f3e6cecee54664cdc434a07676ed25214f399f3
NoNormalDev <190293561+NoNormalDev@users.noreply.github.com>
2026-07-29 16:08:18 +0200

    TextEdit: Scale tab icon with text size

--- scene/gui/text_edit.cpp
+++ scene/gui/text_edit.cpp
@@ -1793,8 +1793,10 @@ void TextEdit::_notification(int p_what) {
 							}
 
 							if (draw_tabs && ((glyphs[j].flags & TextServer::GRAPHEME_IS_TAB) == TextServer::GRAPHEME_IS_TAB)) {
-								int yofs = (text_height - theme_cache.tab_icon->get_height()) / 2 - ldata->get_line_ascent(line_wrap_index);
-								theme_cache.tab_icon->draw(text_ci, Point2(char_pos, ofs_y + yofs), gl_color);
+								const float tab_icon_ratio = 8.0f / 18.0f;
+								float tab_icon_scale = (text_height * tab_icon_ratio) / float(theme_cache.tab_icon->get_height());
+								int yofs = (text_height - theme_cache.tab_icon->get_height() * tab_icon_scale) / 2 - ldata->get_line_ascent(line_wrap_index);
+								theme_cache.tab_icon->draw_rect(text_ci, Rect2(char_pos, ofs_y + yofs, theme_cache.tab_icon->get_height() * tab_icon_scale, theme_cache.tab_icon->get_height() * tab_icon_scale), false, gl_color);
 							} else if (draw_spaces && ((glyphs[j].flags & TextServer::GRAPHEME_IS_SPACE) == TextServer::GRAPHEME_IS_SPACE) && ((glyphs[j].flags & TextServer::GRAPHEME_IS_VIRTUAL) != TextServer::GRAPHEME_IS_VIRTUAL)) {
 								int yofs = (text_height - theme_cache.space_icon->get_height()) / 2 - ldata->get_line_ascent(line_wrap_index);
 								int xofs = (glyphs[j].advance * glyphs[j].repeat - theme_cache.space_icon->get_width()) / 2;
